6abf50d9e67ea71639717e95ffa122a4b0979b1c
steventhanna <steventhanna@gmail.com>
2017-07-19 11:12:05 -0500

    Missed an = sign

--- api/controllers/TeamController.js
+++ api/controllers/TeamController.js
@@ -230,10 +230,10 @@ module.exports = {
     var post = req.body;
     var tempID = post.id;
     delete post.id;
-    if (post.logs == undefined || post.logs.length = 0) {
+    if (post.logs == undefined || post.logs.length == 0) {
       delete post.logs;
     }
-    if (post.players == undefined || post.players.length = 0) {
+    if (post.players == undefined || post.players.length == 0) {
       delete post.players;
     }
     Team.update({
