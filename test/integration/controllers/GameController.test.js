f820ec427d41af9e4b10d09d498ca9a7f23f6b83
David Sa <davidsa03@gmail.com>
2017-07-20 23:34:28 -0400

    nested teamIDs to account for multiple done()

--- test/integration/controllers/GameController.test.js
+++ test/integration/controllers/GameController.test.js
@@ -145,27 +145,26 @@ describe("Game Controller", function() {
               done(err);
             } else {
               assert.include(homeTeam.games, game.id);
-              done();
-            }
-          });
-        }
-      });
-    });
-    it("should have added the game to the away team", function(done) {
-      Team.findOne({
-        teamID: "67890"
-      }).exec(function(err, team) {
-        if (err || team == undefined) {
-          done(err);
-        } else {
-          Game.findOne({
-            gameID: "54321"
-          }).exec(function(err, game) {
-            if (err || game == undefined) {
-              done(err);
-            } else {
-              assert.include(awayTeam.games, game.id);
-              done();
+              it("should have added the game to the away team", function(done) {
+                Team.findOne({
+                  teamID: "67890"
+                }).exec(function(err, team) {
+                  if (err || team == undefined) {
+                    done(err);
+                  } else {
+                    Game.findOne({
+                      gameID: "54321"
+                    }).exec(function(err, game) {
+                      if (err || game == undefined) {
+                        done(err);
+                      } else {
+                        assert.include(awayTeam.games, game.id);
+                        done();
+                      }
+                    });
+                  }
+                });
+              });              
             }
           });
         }
