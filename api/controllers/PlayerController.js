0fd56fd67cac588af681871e65c5b0021159aef1
David Sa <davidsa03@gmail.com>
2017-08-02 11:01:37 -0400

    missing )

--- api/controllers/PlayerController.js
+++ api/controllers/PlayerController.js
@@ -477,7 +477,7 @@ module.exports = {
       },
       function(callback) {
         video.date = game.date;
-        while (video.date.contains("/") {
+        while (video.date.contains("/")) {
            video.date.replace("/", ".");
         }
         video.teamID = game.homeTeamID;
